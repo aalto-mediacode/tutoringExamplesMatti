916b1bdd787654f0bd6a689dbece915e0090d45b
Matti Niinimäki <m@mansteri.com>
2020-05-12 23:33:50 +0300

    Added followMouse example

---
+++ followMouse/src/ofApp.cpp
@@ -0,0 +1,104 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	//ofSetBackgroundAuto(false);
+	pos = { ofRandomWidth(), ofRandomHeight() };
+	vel = { 0.0,0.0 };
+	acc = { 0.0,0.0 };
+	offsets = { ofRandom(1000), ofRandom(1000) };
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+	float timer = ofGetElapsedTimef();
+	float mult = 10.0;
+	noise = { mult*ofSignedNoise(offsets.x + timer), mult*ofSignedNoise(offsets.y + timer) };
+
+	if (ofGetMousePressed()) {
+		target = { mouseX, mouseY };
+	}
+	else {
+		target = { ofGetWidth() / 2, ofGetHeight() / 2 };
+	}
+	
+	// get the direction vector
+	target = target - pos;
+
+	// normalize the vector
+	glm::normalize(target);
+
+	//scale the vector
+	target *= 0.001;
+
+	acc = target;
+
+	vel = vel + acc;
+	pos = pos + vel;
+    pos = pos + noise;
+
+	if (glm::length(vel) > 5.0) {
+		vel = glm::normalize(vel) * 5.0;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	ofDrawCircle(pos, 30);
+	//ofDrawRectangle(pos, 1, 1);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ followMouse/src/ofApp.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+		glm::vec2 target;
+		glm::vec2 pos;
+		glm::vec2 vel;
+		glm::vec2 acc;
+		glm::vec2 noise;
+		glm::vec2 offsets;
+};
